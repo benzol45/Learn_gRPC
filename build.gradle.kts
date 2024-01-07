2fb50aaf621823d3797841a1787759a5223bf07d
Михаил Калинин <benzol.45@gmail.com>
2024-01-07 21:38:17 +0100

    added links to resources

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
 //Мануал от гугла специально по котлину + проект
+//Генерация классов - в проекте по ссылке Maven / Gradle Plugin instructions
+//Описание сервиса на сервере через корутины
 //https://cloud.google.com/blog/products/application-development/use-grpc-with-kotlin
 //https://github.com/grpc/grpc-kotlin?tab=readme-ov-file
+
+//Настройка сервера и клиента
+//https://medium.com/swlh/using-grpc-with-kotlin-for-building-microservices-2a79d95a236a
 
 //Как прописать путь до файлов .proto https://stackoverflow.com/questions/66794075/how-to-specify-the-protobuf-path-using-protobuf-gradle-plugin
 
 //Подключение в спринг https://piotrminkowski.com/2023/08/29/introduction-to-grpc-with-spring-boot/
+
+//Настройка шифрования https://itnext.io/how-to-setup-and-test-tls-in-grpc-grpc-web-1b67cc4413e6
+
+//Правильная обработка ошибок https://www.baeldung.com/grpcs-error-handling
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -25,7 +34,11 @@ dependencies {
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
     // https://mvnrepository.com/artifact/com.google.protobuf/protobuf-kotlin
     implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
+    // https://mvnrepository.com/artifact/io.grpc/grpc-netty
+    implementation("io.grpc:grpc-netty:1.60.1")
 
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
     testImplementation(kotlin("test"))
 }
