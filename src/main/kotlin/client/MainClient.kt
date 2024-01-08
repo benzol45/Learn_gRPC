ea087d93242881794333401648ba41b1f8cf8190
Михаил Калинин <benzol.45@gmail.com>
2024-01-08 16:00:16 +0100

    added error responding & handling

--- src/main/kotlin/client/MainClient.kt
+++ src/main/kotlin/client/MainClient.kt
@@ -2,6 +2,7 @@ package client
 
 fun main(args: Array<String>) {
     val client = GrpcClient()
-    client.processSending("test name")
-    client.processStreaming()
+    //client.processSending("test name")
+    //client.processStreaming()
+    client.processSending("") //With error in the result
 }
